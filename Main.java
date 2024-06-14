7c45f73ba22439c56911492ae6242cb6c9cd3731
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-06-13 21:45:37 -0500

    implementa la funcion para construir un arbol en base al valor ascii de las letras de una palabra

--- Main.java
+++ Main.java
@@ -3,9 +3,10 @@ public class Main {
    public static void main(String[] args) {
       AVLTree<Integer> tree = new AVLTree<>();
       Scanner sc = new Scanner(System.in);
+      System.out.println("Ingrese la palabra");
       String word = sc.nextLine();
       for(int i = 0; i < word.length(); i++){
-         char aux
+         tree.insert((int)(word.charAt(i)));
       }
 
       tree.preOrder();
